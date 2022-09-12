d11113f7e2b37222ec33a5d1eebf15f6bfdb1bb4
Praveen <73181035+Praveensarathy@users.noreply.github.com>
2022-09-12 11:02:48 +0530

    left_Rotation on 1D array

---
+++ DataStructure/Arrays/Leftrotation.java
@@ -0,0 +1,40 @@
+class Result {
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        int i,j=0;
+        for(i=0;i<d;i++){
+            int in =arr.remove(j);
+            arr.add(in);
+        }
+        return arr;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.rotateLeft(d, arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
