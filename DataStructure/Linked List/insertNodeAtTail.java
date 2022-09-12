27191932d55415558309b7df24382c32b6d6b33e
Praveen <73181035+Praveensarathy@users.noreply.github.com>
2022-09-12 14:25:38 +0530

    insertNodeAtTail in Linked list
    https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true&h_r=next-challenge&h_v=zen

---
+++ DataStructure/Linked List/insertNodeAtTail.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        
+        SinglyLinkedListNode a = head;
+        SinglyLinkedListNode newnode = new SinglyLinkedListNode(data) ;
+        if(head == null){
+            head = newnode;
+        }
+        else{
+            while(a.next!=null){
+                a = a.next;
+            }
+            a.next = newnode;
+        }
+        return head;
+        
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
